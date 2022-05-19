331d4cc20809cab59fb386a779587cceffbe301c
Elebesova Gulzat <88238675+guli02@users.noreply.github.com>
2022-05-19 13:51:06 +0600

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,7 @@
+//Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+
+let uzerName = 'Gulzat';
+let uzerMaile = 'gulzat.elebesova.kk@gmail.com';
+
+alert("Имя пользователя: " + uzerName);
+alert("Почта пользователя: " + uzerMaile);
